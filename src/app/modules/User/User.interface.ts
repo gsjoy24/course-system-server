5c3c59f4dbf2c7c7ff884c903788ea1cfc853945
gsjoy24 <gour.joy24@gmail.com>
2023-12-25 22:41:16 +0600

    added user model and interface

---
+++ src/app/modules/User/User.interface.ts
@@ -0,0 +1,6 @@
+export type TUser = {
+  username: string;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+};
---
+++ src/app/modules/User/User.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './User.interface';
+
+const UserSchema = new Schema<TUser>({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+export const User = model<TUser>('User', UserSchema);
